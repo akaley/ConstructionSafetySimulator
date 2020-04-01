60b79ed4c8ae9b9eec84a8728de288dc60457a73
Ben Hibler <benhibler@gmail.com>
2020-04-01 13:48:50 -0700

    added truck assets
    Animations not currently working

---
+++ ConstructionSafetySimulator/Assets/animTruck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animTruck : MonoBehaviour
+
+    
+{
+
+    public Animator myanim;
+    Renderer rend;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myanim = GetComponent<Animator>();
+        rend = GetComponent<Renderer>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            myanim.SetBool("move", true);
+        }
+    }
+}
